11b564c17083ca39e0d4193eeea8743d65890236
RaulAnselmoDSantos <raul.trabalho5511@gmail.com>
2024-03-04 17:25:18 -0300

    feat: ajustments of the page : TelaLogin

---
+++ site-olheoleo/src/app/TelaLogin/page.tsx
@@ -0,0 +1,10 @@
+import '../globals.css';
+
+
+export default function TelaLogin(){
+    return(
+        <main className='bg-white text-black'>
+            <p>Olá mundo</p>
+        </main>
+    )
+}
