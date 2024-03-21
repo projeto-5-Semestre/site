600f8caa0a4c99e0e6200053bb2fd6132d9c20e2
RaulAnselmoDSantos <raul.trabalho5511@gmail.com>
2024-03-21 09:51:55 -0300

    feat: components of the admin authenticator

--- site-olheoleo/src/app/page.tsx
+++ site-olheoleo/src/app/page.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {s
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
       <p className="text-red-950"> 
