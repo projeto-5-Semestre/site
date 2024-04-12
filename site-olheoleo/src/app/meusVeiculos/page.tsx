135c3d292ad64a84205fb75eea2c5c75753f157f
joaoizidioLoiola <joaoizidio287@gmail.com>
2024-04-12 18:02:29 -0300

    Ajustando Button ADD_Veiculo

--- site-olheoleo/src/app/meusVeiculos/page.tsx
+++ site-olheoleo/src/app/meusVeiculos/page.tsx
@@ -1,11 +1,8 @@
 "use client"
-import { useEffect, useState } from "react";
-
-
-import Image from 'next/image';
-import Modal from '@/components/Modal'
-
-import sempng from '../../../public/sem.jpg.gif'
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Modal from "@/components/Modal";
+import Modal_AddVeiculos from "@/components/Modal_AddVeiculos";
 import Link from "next/link";
 import { IoIosAddCircleOutline } from "react-icons/io";
 
@@ -16,48 +13,54 @@ export type Veiculo = {
   placa: string,
 }
 
-
 export default function Main() {
   const [data, setData] = useState<{ veiculos: Veiculo[] }>({ veiculos: [] });
   const [selectedVeiculo, setSelectedVeiculo] = useState<Veiculo | null>(null);
-  const [openModal, setOpenModal] = useState(false);
+  const [openModalVeiculos, setOpenModalVeiculos] = useState(false);
+  const [openModalAddVeiculo, setOpenModalAddVeiculo] = useState(false);
 
   useEffect(() => {
-    fetch('api/veiculos')
+    fetch("api/veiculos")
       .then((res) => res.json())
-      .then((data) => setData(data.data))
+      .then((data) => setData(data.data));
   }, []);
 
   useEffect(() => {
-    if (openModal) {
-      document.body.classList.add('overflow-hidden');
+    if (openModalVeiculos || openModalAddVeiculo) {
+      document.body.classList.add("overflow-hidden");
     } else {
-      document.body.classList.remove('overflow-hidden');
+      document.body.classList.remove("overflow-hidden");
     }
-  }, [openModal]);
+  }, [openModalVeiculos, openModalAddVeiculo]);
 
-  let handleOpenModal = (veiculo: Veiculo) => {
+  let handleOpenModalVeiculos = (veiculo: Veiculo) => {
     setSelectedVeiculo(veiculo);
-    setOpenModal(true);
+    setOpenModalVeiculos(true);
   };
 
-  let handleCloseModal = () => {
+  let handleCloseModalVeiculos = () => {
     setSelectedVeiculo(null);
-    setOpenModal(false);
+    setOpenModalVeiculos(false);
+  };
+
+  let handleCloseModalAddVeiculo = () => {
+    setOpenModalAddVeiculo(false);
   };
 
   return (
     <main className="flex flex-col min-w-screen min-h-screen bg-fund">
       <header className="flex flex-row min-w-screen justify-between bg-fund ">
         <div className="flex justify-start items-start w-screen px-10 py-10 text-txt font-semibold ">
-          <h1>
-            SEUS VEÍCULOS
-          </h1>
+          <h1>SEUS VEÍCULOS</h1>
         </div>
       </header>
       <div className="flex flex-col space-y-4 items-center w-full p-3 m-1 bg-fund">
         {data.veiculos.map((veiculo, index) => (
-          <div key={index} className="min-w-[80%] max-w-[75%] sm:min-w-[80%] md:w-[35%] md:h-[15%] md:max-w-[65%] items-center grid grid-rows-1 gap-4 bg-grid rounded-lg shadow-sm hover:shadow-md hover:shadow-slate-400/60 cursor-pointer" onClick={() => handleOpenModal(veiculo)}>
+          <div
+            key={index}
+            className="min-w-[80%] max-w-[75%] sm:min-w-[80%] md:w-[35%] md:h-[15%] md:max-w-[65%] items-center grid grid-rows-1 gap-4 bg-grid rounded-lg shadow-sm hover:shadow-md hover:shadow-slate-400/60 cursor-pointer"
+            onClick={() => handleOpenModalVeiculos(veiculo)}
+          >
             <div className="flex flex-row items-center justify-around mt-3 max-h-[75%] ">
               <div className="flex items-center justify-center font-bold text-txt min-w-[140px] max-w-[155px]">
                 <p className="p-3">{veiculo.modelo}</p>
@@ -65,10 +68,11 @@ export default function Main() {
               <div className="">
                 <Image
                   className="object-contain rounded-lg min-w-[65%] min-h-[65%] max-h-[65%] max-w-[75%]"
-                  src={veiculo.url_imagem || sempng}
+                  src={veiculo.url_imagem || "/sem.jpg.gif"}
                   width={250}
                   height={250}
-                  alt="Carro" />
+                  alt="Carro"
+                />
               </div>
             </div>
             <div className="flex items-center justify-around w-full h-7 mb-4 font-bold text-txt">
@@ -77,27 +81,18 @@ export default function Main() {
             </div>
           </div>
         ))}
-        {openModal && (
-          <div className="fixed inset-0 backdrop-blur-sm pointer-events-none z-10"> </div>
-        )}
-        <Modal isOpen={openModal} onClose={handleCloseModal} image={selectedVeiculo?.url_imagem} >
-          {
-            selectedVeiculo && (
-              <div className="text-center">
-                <h2>{selectedVeiculo.modelo}</h2>
-                <p>Quilometragem: {selectedVeiculo.quilometragem}</p>
-                <p>Placa: {selectedVeiculo.placa}</p>
-              </div>
-            )}
-        </Modal>
-        <Link href="/addCar">
-          <button className="fixed bottom-2 right-3 bg-shad text-txt py-2 px-3 rounded-xl">
-            <span className="text-txt text-3xl">
-              <IoIosAddCircleOutline />
-            </span>
-          </button>
-        </Link>
+        <Modal_AddVeiculos isOpen={openModalAddVeiculo} onClose={handleCloseModalAddVeiculo} />
+        <button
+          onClick={() => setOpenModalAddVeiculo(true)}
+          className="fixed bottom-20 right-3 bg-shad text-txt py-3 px-3 rounded-xl"
+        >
+          <span className="text-txt text-3xl">
+            <IoIosAddCircleOutline />
+          </span>
+        </button>
       </div>
     </main>
-  )
+  );
 }
+
+
--- site-olheoleo/src/components/Modal_AddVeiculos.tsx
+++ site-olheoleo/src/components/Modal_AddVeiculos.tsx
@@ -1,10 +1,15 @@
-'use client'
+"use client"
 import React, { useState } from 'react';
 import Image from 'next/image';
 import Button_AddFoto from './Button_AddFoto';
+import { IoCloseCircle } from 'react-icons/io5';
 
-export default function Modal_AddVeiculos() {
-  const [showModal, setShowModal] = useState(false);
+interface Modal_AddVeiculosProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export default function Modal_AddVeiculos({ isOpen, onClose }: Modal_AddVeiculosProps) {
   const [model, setModel] = useState('');
   const [plate, setPlate] = useState('');
   const [mileage, setMileage] = useState('');
@@ -28,7 +33,7 @@ export default function Modal_AddVeiculos() {
     setMileage('');
     setOilType('');
     setAddClicked(false);
-    setShowModal(false);
+    onClose(); // Fechando o modal após adicionar o veículo
   };
 
   const getBorderColor = (value: string) => {
@@ -43,26 +48,31 @@ export default function Modal_AddVeiculos() {
 
   return (
     <>
-      <div onClick={() => setShowModal(true)}>Adicionar Veículo</div>
-      {showModal && (
+      {isOpen && (
         <div className="fixed z-10 inset-0 overflow-y-auto">
           <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
             <div className="fixed inset-0 transition-opacity" aria-hidden="true">
               <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
             </div>
-            <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">#8203;</span>
+            <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+              &#8203;
+            </span>
             <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
               <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-
-                <h2 className="text-lg leading-6 font-medium text-gray-900">Adicionar Novo Veículo <span className="flex ml-auto justify-end close cursor-pointer text-fund" onClick={() => setShowModal(false)}>X</span>
+                <h2 className="text-lg leading-6 font-medium text-gray-900">
+                  Adicionar Novo Veículo{' '}
+                  <span className="flex ml-auto justify-end close cursor-pointer text-fund" onClick={onClose}>
+                    <IoCloseCircle size={28} />
+                  </span>
                 </h2>
 
-
-                <Image src={selectedImage}
+                <Image
+                  src={selectedImage}
                   alt="Car Illustration"
                   className="w-full h-auto animate-flip-once"
                   width={500}
-                  height={300} />
+                  height={300}
+                />
 
                 <Button_AddFoto selectedImage={selectedImage} setSelectedImage={setSelectedImage} />
 
@@ -96,7 +106,10 @@ export default function Modal_AddVeiculos() {
                   className={`mt-3 p-2 w-full border ${getBorderColor(oilType)} rounded-md text-black`}
                 />
 
-                <button onClick={handleAddVehicle} className="mt-3 w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-pointer">
+                <button
+                  onClick={handleAddVehicle}
+                  className="mt-3 w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-pointer"
+                >
                   Adicionar Veículo
                 </button>
               </div>
