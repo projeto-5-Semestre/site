2f924b7a4caa7e46ad35e8a14180b16d89909ff4
joaoizidioLoiola <joaoizidio287@gmail.com>
2024-04-12 18:30:08 -0300

    Correcao

--- site-olheoleo/src/app/meusVeiculos/page.tsx
+++ site-olheoleo/src/app/meusVeiculos/page.tsx
@@ -81,6 +81,20 @@ export default function Main() {
             </div>
           </div>
         ))}
+        {openModalVeiculos && (
+          <div className="fixed inset-0 backdrop-blur-sm pointer-events-none z-10"> </div>
+        )}
+        <Modal isOpen={openModalVeiculos} onClose={handleCloseModalVeiculos} image={selectedVeiculo?.url_imagem} >
+          {
+            selectedVeiculo && (
+              <div className="text-center">
+                <h2>{selectedVeiculo.modelo}</h2>
+                <p>Quilometragem: {selectedVeiculo.quilometragem}</p>
+                <p>Placa: {selectedVeiculo.placa}</p>
+              </div>
+            )}
+        </Modal>
+
         <Modal_AddVeiculos isOpen={openModalAddVeiculo} onClose={handleCloseModalAddVeiculo} />
         <button
           onClick={() => setOpenModalAddVeiculo(true)}
